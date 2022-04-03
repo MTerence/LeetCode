a2e6ce0202f66dead1ae24d44372ebb375c78665
Terence <machao@yiruikecorp.com>
2022-04-03 18:17:25 +0800

    [226. 翻转二叉树] 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。  https://leetcode-cn.com/problems/invert-binary-tree/

---
+++ LeetCode/Tree/InvertTree.swift
@@ -0,0 +1,31 @@
+//
+//  InvertTree.swift
+//  LeetCode
+//
+//  Created by Ternence on 2022/4/3.
+//
+
+/*
+ 226. 翻转二叉树
+ 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ 示例 1：
+ 输入：root = [4,2,7,1,3,6,9]
+ 输出：[4,7,2,9,6,3,1]
+ https://leetcode-cn.com/problems/invert-binary-tree/
+ */
+import Cocoa
+
+class InvertTree: NSObject {
+    
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        if root == nil { return root }
+        
+        let left = invertTree(root!.left)
+        let right = invertTree(root!.right)
+        
+        root!.left = right
+        root!.right = left
+        return root
+    }
+
+}
--- README.md
+++ README.md
@@ -103,3 +103,6 @@
  给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
  百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
  https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ 
+ ###  226. 翻转二叉树 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ https://leetcode-cn.com/problems/invert-binary-tree/
