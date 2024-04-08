954682764253acc4bb71adda0a99baa2989cfa9b
machao07 <machao07@kuaishou.com>
2024-04-08 14:32:45 +0800

    [leetcode] 最长重复子串

---
+++ LeetCode/Hard/LongestDupSubstring.swift
@@ -0,0 +1,24 @@
+//
+//  LongestDupSubstring.swift
+//  LeetCode
+//
+//  Created by 马超 on 2024/4/8.
+//
+/*
+ 1044. 最长重复子串 困难
+ 给你一个字符串 s ，考虑其所有 重复子串 ：即 s 的（连续）子串，在 s 中出现 2 次或更多次。这些出现之间可能存在重叠。
+ 返回 任意一个 可能具有最长长度的重复子串。如果 s 不含重复子串，那么答案为 "" 。
+
+ 示例 1：
+ 输入：s = "banana"
+ 输出："ana"
+ 示例 2：
+ 输入：s = "abcd"
+ 输出：""
+ https://leetcode.cn/problems/longest-duplicate-substring/description/
+ */
+import Cocoa
+
+class LongestDupSubstring: NSObject {
+
+}
--- README.md
+++ README.md
@@ -108,6 +108,11 @@ https://leetcode.cn/problems/chou-shu-lcof/
 ##### 接雨水
 https://leetcode.cn/problems/trapping-rain-water/
 
+##### 1044. 最长重复子串
+给你一个字符串 s ，考虑其所有 重复子串 ：即 s 的（连续）子串，在 s 中出现 2 次或更多次。这些出现之间可能存在重叠。
+ 返回 任意一个 可能具有最长长度的重复子串。如果 s 不含重复子串，那么答案为 "" 。
+https://leetcode.cn/problems/longest-duplicate-substring/description/
+
 ##### 最短循环子串
 
 # ListNode
